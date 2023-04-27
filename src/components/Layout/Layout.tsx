201d382a8192d3193304109539ddacf9d98b9b91
Jim Huang <jim30223@gmail.com>
2023-04-27 23:21:41 +0800

    fix: on detail page hide the menu

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -4,12 +4,13 @@ import { LabelOptionEnum, LabelTabEnum } from '@src/enums/labelEnum';
 import { Menu } from 'antd';
 import Layout, { Content, Footer, Header } from 'antd/es/layout/layout';
 import { MenuInfo } from 'rc-menu/lib/interface';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { Breadcrumb } from './Breadcrumb';
 import styled from './Layout.module.scss';
 
 export const LayoutElement = () => {
   const { selectTab } = useIssuesContext();
+  const pathname = useLocation().pathname;
 
   const navClickHandler = ({ key }: MenuInfo) => {
     switch (key) {
@@ -33,18 +34,22 @@ export const LayoutElement = () => {
       <Layout className={styled.container}>
         <Header>
           <div className="logo" />
-          <Menu
-            theme="dark"
-            mode="horizontal"
-            defaultSelectedKeys={['0']}
-            onClick={navClickHandler}
-            items={labelData.map((label, i) => {
-              return {
-                key: i,
-                label,
-              };
-            })}
-          />
+          {pathname.includes('detail') ? (
+            ''
+          ) : (
+            <Menu
+              theme="dark"
+              mode="horizontal"
+              defaultSelectedKeys={['0']}
+              onClick={navClickHandler}
+              items={labelData.map((label, i) => {
+                return {
+                  key: i,
+                  label,
+                };
+              })}
+            />
+          )}
         </Header>
         <Content className={styled.content}>
           <Breadcrumb />
